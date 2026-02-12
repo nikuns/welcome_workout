2c39fe794e4ba8df65351f31be90bbc0f7470dd1
nikuns <nikodemgotfryd@gmail.com>
2026-02-12 21:11:50 +0100

    added relations to schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,3 +1,4 @@
+import { Many, relations } from "drizzle-orm";
 import {
   sqliteTable,
   text,
@@ -5,38 +6,68 @@ import {
   SQLiteTable,
 } from "drizzle-orm/sqlite-core";
 
+// TABLES
 export const exercise = sqliteTable("exercise", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   name: text("name").notNull(),
   description: text("description"),
 });
 
-export const workout_template = sqliteTable("workout_template", {
+export const workoutTemplate = sqliteTable("workout_template", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   name: text("name").notNull(),
   description: text("description"),
 });
 
-export const workout_exercise = sqliteTable("workout_exercise", {
+export const workoutExercise = sqliteTable("workoutExercise", {
   id: integer("id").primaryKey({ autoIncrement: true }),
-  workout_template_id: integer("workout_template_id")
+  workoutTemplateId: integer("workout_template_id")
     .notNull()
-    .references(() => workout_template.id),
-  exercise_id: integer("exercise_id")
+    .references(() => workoutTemplate.id),
+  exerciseId: integer("exercise_id")
     .notNull()
     .references(() => exercise.id),
   position: integer("position").notNull(),
   notes: text("notes"),
 });
 
+// RELATIONS
+
+export const exercisesRelations = relations(exercise, ({ many }) => ({
+  workoutExercises: many(workoutExercise),
+}));
+
+export const workoutRelations = relations(workoutTemplate, ({ many }) => ({
+  workoutExercises: many(workoutExercise),
+}));
+
+export const workoutExercisesRelations = relations(
+  workoutExercise,
+  ({ one }) => ({
+    workoutTemplate: one(workoutTemplate, {
+      fields: [workoutExercise.workoutTemplateId],
+      references: [workoutTemplate.id],
+    }),
+    exercise: one(exercise, {
+      fields: [workoutExercise.exerciseId],
+      references: [exercise.id],
+    }),
+  }),
+);
+
+// TYPES
+
 export type Exercise = typeof exercise.$inferSelect;
 export type newExercise = typeof exercise.$inferInsert;
 
-export type Workout = typeof workout_template.$inferInsert;
-export type newWorkout = typeof workout_template.$inferInsert;
+export type Workout = typeof workoutTemplate.$inferSelect;
+export type newWorkout = typeof workoutTemplate.$inferInsert;
+
+export type WorkoutExercise = typeof workoutExercise.$inferSelect;
+export type newWorkoutExercise = typeof workoutExercise.$inferInsert;
 
 export const schema = {
   exercise,
-  workout_template,
-  workout_exercise,
+  workoutTemplate,
+  workoutExercise,
 } as const;
--- db/workoutRepository.ts
+++ db/workoutRepository.ts
@@ -1,34 +1,70 @@
-import { ExpoSQLiteDatabase, useLiveQuery } from "drizzle-orm/expo-sqlite";
-import { workout_template, Workout, newWorkout } from "./schema";
-import useWorkouts from "@/hooks/useWorkout";
 import { eq } from "drizzle-orm";
+import { ExpoSQLiteDatabase, useLiveQuery } from "drizzle-orm/expo-sqlite";
+import {
+  exercise,
+  newWorkout,
+  Workout,
+  workoutExercise,
+  WorkoutExercise,
+  workoutTemplate,
+} from "./schema";
 
-type DB = ExpoSQLiteDatabase<{ workout_template: typeof workout_template }>;
+type DB = ExpoSQLiteDatabase<{ workoutTemplate: typeof workoutTemplate }>;
 
 export const createWorkoutRepository = (db: DB) => {
   return {
     useAllWorkouts() {
-      return useLiveQuery(db.select().from(workout_template));
+      return useLiveQuery(db.select().from(workoutTemplate));
     },
 
     useWorkoutById(id: number) {
       return useLiveQuery(
-        db.query.workout_template.findFirst({
-          where: eq(workout_template.id, id),
+        db.query.workoutTemplate.findFirst({
+          where: eq(workoutTemplate.id, id),
         }),
+      );
+    },
+
+    useExercisesInWorkout(workoutId: number) {
+      return useLiveQuery(
+        db
+          .select({
+            // pola z workout_exercise
+            weId: workoutExercise.id,
+            position: workoutExercise.position,
+            notes: workoutExercise.notes,
+            // pola z exercise
+            exerciseId: exercise.id,
+            exerciseName: exercise.name,
+            exerciseDescription: exercise.description,
+          })
+          .from(workoutExercise)
+          .innerJoin(exercise, eq(workoutExercise.exerciseId, exercise.id))
+          .where(eq(workoutExercise.workoutTemplateId, workoutId))
+          .orderBy(workoutExercise.position),
       );
     },
 
     async createWorkout(data: newWorkout): Promise<Workout> {
       const [inserted] = await db
-        .insert(workout_template)
+        .insert(workoutTemplate)
         .values({ ...data })
         .returning();
       return inserted;
     },
 
     async deleteWorkout(id: number): Promise<void> {
-      await db.delete(workout_template).where(eq(workout_template.id, id));
+      await db.delete(workoutTemplate).where(eq(workoutTemplate.id, id));
+    },
+
+    async addExerciseToWorkout(
+      data: WorkoutExercise,
+    ): Promise<WorkoutExercise> {
+      const [inserted] = await db
+        .insert(workoutExercise)
+        .values({ ...data })
+        .returning();
+      return inserted;
     },
   };
 };
