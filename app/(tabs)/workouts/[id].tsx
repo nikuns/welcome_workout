ae3e8143fedfcd0bd6147eac8264b1c5317963d1
nikuns <nikodemgotfryd@gmail.com>
2026-03-11 23:01:23 +0100

    moved modal from workout details to a separate component

--- app/(tabs)/workouts/[id].tsx
+++ app/(tabs)/workouts/[id].tsx
@@ -1,44 +1,25 @@
-import useWorkouts from "@/hooks/useWorkout";
-import { Link, useLocalSearchParams } from "expo-router";
-import React, { useState } from "react";
+import ExerciseItem from "@/components/ExerciseItem";
 import WorkoutExerciseItem from "@/components/WorkoutExerciseItem";
+import useExercises from "@/hooks/useExercise";
+import useWorkouts from "@/hooks/useWorkout";
+import { useLocalSearchParams } from "expo-router";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 import {
   Alert,
   Button,
   FlatList,
   Modal,
   Pressable,
-  SafeAreaView,
   Text,
   View,
 } from "react-native";
-import useExercises from "@/hooks/useExercise";
-import ExerciseItem from "@/components/ExerciseItem";
-import { exercise } from "@/db/schema";
+import ExerciseListModal from "@/components/ExerciseListModal";
 
 export default function workoutDetail() {
   const { id } = useLocalSearchParams<{ id: string }>();
   const { useExercisesInWorkout, addExerciseToWorkout } = useWorkouts();
-  const { exercises } = useExercises();
-  const [modalVisible, setModalVisible] = useState(false);
   const { data: workoutExercises, error } = useExercisesInWorkout(Number(id));
-
-  const handleNewExerciseInWorkout = async (
-    exerciseId: number,
-  ): Promise<void> => {
-    try {
-      await addExerciseToWorkout({
-        workoutTemplateId: Number(id),
-        exerciseId: exerciseId,
-        position: workoutExercises.length + 1,
-      });
-      setModalVisible(false);
-      Alert.alert("Dodano cwiczenie do treningu");
-    } catch (e) {
-      console.log(e);
-      Alert.alert("Blad wczytywania cwiczenia");
-    }
-  };
 
   if (!workoutExercises) {
     return null;
@@ -62,26 +43,7 @@ export default function workoutDetail() {
           <WorkoutExerciseItem workoutExercise={item} />
         )}
       />
-      <Button
-        onPress={() => setModalVisible(true)}
-        title="Add exercise"
-      ></Button>
-
-      <Modal visible={modalVisible} animationType="slide">
-        <SafeAreaView className="bg-slate 300 flex-1">
-          <FlatList
-            className="bg-background"
-            data={exercises}
-            keyExtractor={(item) => item.id.toString()}
-            renderItem={({ item }) => (
-              <Pressable onPress={() => handleNewExerciseInWorkout(item.id)}>
-                <ExerciseItem exercise={item} />
-              </Pressable>
-            )}
-          />
-          <Button onPress={() => setModalVisible(false)} title="Close"></Button>
-        </SafeAreaView>
-      </Modal>
+      <ExerciseListModal id={id!} workoutExercises={workoutExercises} />
     </View>
   );
 }
---
+++ components/ExerciseListModal.tsx
@@ -0,0 +1,72 @@
+import exercises from "@/app/(tabs)/exercises";
+import {
+  Modal,
+  SafeAreaView,
+  FlatList,
+  Pressable,
+  Button,
+  Alert,
+} from "react-native";
+import ExerciseItem from "./ExerciseItem";
+import { useState } from "react";
+import useExercises from "@/hooks/useExercise";
+import useWorkouts from "@/hooks/useWorkout";
+import { WorkoutExercise } from "@/db/schema";
+
+type ExerciseListModalProps = {
+  id: string;
+  workoutExercises: WorkoutExercise[];
+};
+
+// TODO
+// Popraw ten modal, zeby stan mogl byc kontrolowany z zewntrz
+export default function ExerciseListModal({
+  id,
+  workoutExercises,
+}: ExerciseListModalProps) {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const { addExerciseToWorkout } = useWorkouts();
+  const { exercises } = useExercises();
+
+  const handleNewExerciseInWorkout = async (
+    exerciseId: number,
+  ): Promise<void> => {
+    try {
+      await addExerciseToWorkout({
+        workoutTemplateId: Number(id),
+        exerciseId: exerciseId,
+        position: workoutExercises.length + 1,
+      });
+      setModalVisible(false);
+      Alert.alert("Dodano cwiczenie do treningu");
+    } catch (e) {
+      console.log(e);
+      Alert.alert("Blad wczytywania cwiczenia");
+    }
+  };
+
+  return (
+    <>
+      <Modal visible={modalVisible} animationType="slide">
+        <SafeAreaView className="bg-background ">
+          <FlatList
+            className="bg-background"
+            data={exercises}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
+              <Pressable onPress={() => handleNewExerciseInWorkout(item.id)}>
+                <ExerciseItem exercise={item} />
+              </Pressable>
+            )}
+          />
+          <Button onPress={() => setModalVisible(false)} title="Close"></Button>
+        </SafeAreaView>
+      </Modal>
+      <Button
+        onPress={() => setModalVisible(true)}
+        title="Add exercise"
+      ></Button>
+    </>
+  );
+}
